ee6eb5b9e681b6ccbc8d919080e7bbe4d8caf735
Artem Kopytok <13ar.bass@gmail.com>
2021-11-15 12:26:48 +0300

    removed: translateDateToGerman

--- blog-list.js
+++ blog-list.js
@@ -6,57 +6,6 @@ function pagOnClickScroll() {
     });
 }
 
-function translateDateToGerman() {
-    let dateLabels = document.querySelectorAll('.body-3--date');
-
-    dateLabels.forEach(label => {
-        splittedLabel = label.textContent.split(' ')
-        currentNonth = splittedLabel[0];
-
-        if (currentNonth == 'Jan') {
-            currentNonth = 'Jan'
-        }
-
-        if (currentNonth == 'Feb') {
-            currentNonth = 'Feb'
-        }
-        if (currentNonth == 'Mar') {
-            currentNonth = 'Mär'
-        }
-        if (currentNonth == 'Apr') {
-            currentNonth = 'Apr'
-        }
-        if (currentNonth == 'May') {
-            currentNonth = 'Mai'
-        }
-        if (currentNonth == 'Jun') {
-            currentNonth = 'Jun'
-        }
-        if (currentNonth == 'Jul') {
-            currentNonth = 'Jul'
-        }
-        if (currentNonth == 'Aug') {
-            currentNonth = 'Aug'
-        }
-        if (currentNonth == 'Sep') {
-            currentNonth = 'Sep'
-        }
-        if (currentNonth == 'Oct') {
-            currentNonth = 'Okt'
-        }
-        if (currentNonth == 'Nov') {
-            currentNonth = 'Nov'
-        }
-        if (currentNonth == 'Dec') {
-            currentNonth = 'Dez'
-        }
-
-
-        label.textContent = (currentNonth + ' ' + splittedLabel[1] + ' ' + splittedLabel[2]);
-    });
-
-}
-
 
 (function () {
     fsComponent2 = new FsLibrary('.cl__grid__media-list-wrapper--press')
@@ -92,7 +41,6 @@ function startFinsweetFilter() {
     }
 
     setTimeout(apperMedia, 500);
-    setTimeout(translateDateToGerman, 400);
     setTimeout(pagOnClickScroll, 600);
 }
 
