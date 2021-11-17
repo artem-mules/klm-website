782cbee66755bb950dca1fe06092726c9088e519
Artem Kopytok <13ar.bass@gmail.com>
2021-11-18 02:33:29 +0300

    before: sleep :)

--- step-form-logic.js
+++ step-form-logic.js
@@ -1,3 +1,8 @@
+let activeStep;
+let nextStep;
+let mainForm = document.querySelector('.form-block');
+let allStepButtons = document.querySelectorAll('.buttons--step');
+let allReqFields;
 let appendWrapperStepButtons = document.querySelector('.steps-wrapper');
 let allSteps = document.querySelectorAll('.step');
 
@@ -19,4 +24,41 @@ allSteps.forEach(function(elem, index) {
 });
 
 
-//отрабатываем клик по шагам
+function showNextStep() {
+    console.log('переводим на следующий шаг');
+    activeStep = document.querySelector('.step--active');
+    nextStep = activeStep.nextElementSibling;
+    console.log(nextStep);
+    activeStep.classList.remove('step--active');
+    nextStep.classList.add('step--active');
+}
+
+//надо проверять поля для активных 
+function checkReqFields() {
+    allReqFields = document.querySelectorAll('.step--active [required]');
+    allReqFields.forEach(reqField => {
+        if (reqField.value == '') {
+            reqField.classList.add('input--error');
+            reqField.classList.remove('input--passed');
+        } else {
+            reqField.classList.remove('input--error');
+            reqField.classList.add('input--passed');
+        }
+    });
+
+    errorsCounter = document.querySelectorAll('.step--active .input--error').length;
+    activeStep = document.querySelector('.step--active');
+
+    if (errorsCounter == 0) {
+        activeStep.setAttribute('step-passed', 'true');
+        showNextStep();
+    } else {
+        activeStep.setAttribute('step-passed', 'false');
+    }
+}
+
+allStepButtons.forEach(stepButton => {
+    stepButton.addEventListener('click', function () {
+        checkReqFields();
+    });
+});
