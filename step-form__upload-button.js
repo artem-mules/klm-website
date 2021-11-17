ce9d7d305afeaa58f1063e1dcd18a63ff721da6f
Artem Kopytok <13ar.bass@gmail.com>
2021-11-18 01:30:24 +0300

    updated: upload button script done

--- step-form__upload-button.js
+++ step-form__upload-button.js
@@ -1,35 +1,47 @@
-function initializeUpCare() {
-    let elButton__uploadCare = document.querySelector('.buttons--uploadcare');
-    let elFileName;
-    let elTrigger__uploadCare = document.querySelector('.uploadcare--widget__button');
-    let nameInterval;
+let uploadCareTrigger;
+let uploadButton = document.querySelector('.buttons--uploadcare');
+let embedChangeCounter = 0;
+let fileNameElement;
 
-    function changeButtonTitle() {
-        elButton__uploadCare.textContent = 'Upload'
+function nameChanged() {
+    fileNameElement = document.querySelector('.uploadcare--widget__file-name');
+    if (fileNameElement != null) {
+        uploadButton.textContent = fileNameElement.textContent;
     }
-
-    function checkFileName() {
-        elFileName = document.querySelector('.uploadcare--widget__file-name');
-        if (elFileName == null) {
-            //do nothing
-        } else {
-            elButton__uploadCare.textContent = ('Your file is: ' + elFileName.textContent);
-            clearInterval(nameInterval);
-        }
-    }
-
-    function checkFileNameInterval() {
-        nameInterval = setInterval(checkFileName, 100)
-    }
-
-    elButton__uploadCare.addEventListener('click', function () {
-        checkFileNameInterval();
-
-        elTrigger__uploadCare.click();
-        elButton__uploadCare.textContent = 'Wait, please...'
-
-        setTimeout(changeButtonTitle, 2000);
-    });
 }
 
-setTimeout(initializeUpCare, 500);
+function listentingUploadButton() {
+    let targetNodeFileNameStatus = document.querySelector('#uploadcare--widget__text');
+    if (targetNodeFileNameStatus != null) {
+        const targetNodeFileName = document.querySelector('#uploadcare--widget__text');
+        const observerOptionsFileName = {
+            childList: true,
+            attributes: true,
+            subtree: true
+        }
+        const observerFileName = new MutationObserver(nameChanged);
+        observerFileName.observe(targetNodeFileName, observerOptionsFileName);
+    }
+
+
+    if (embedChangeCounter == 0) {
+        uploadCareTrigger = document.querySelector('.uploadcare--widget__button');
+        uploadButton.addEventListener('click', function () {
+            uploadCareTrigger.click();
+        })
+    }
+    embedChangeCounter = embedChangeCounter + 1;
+}
+
+function embedChanged() {
+    listentingUploadButton();
+}
+
+const targetNodeEmbed = document.querySelector('.uploadcare-embed');
+const observerOptionsEmbed = {
+    childList: true,
+    attributes: true,
+    subtree: true
+}
+const observerEmbed = new MutationObserver(embedChanged);
+observerEmbed.observe(targetNodeEmbed, observerOptionsEmbed);
