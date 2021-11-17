990d8b2ba6a5c461a220beca6b6369380ca31df5
Artem Kopytok <13ar.bass@gmail.com>
2021-11-18 01:49:16 +0300

    added: countyList js

---
+++ form__countries-list.js
@@ -0,0 +1,14 @@
+let countryListInput = document.querySelector('#step-1__country');
+let statesListInput = document.querySelector('#step-1__state');
+let stateTriggerShow = document.querySelector('.o__input-countries-wrapper__1');
+let stateTriggerClose = document.querySelector('.c__input-countries-wrapper__1');
+
+countryListInput.addEventListener('change', function () {
+    if (this.value == 'Deutschland') {
+        stateTriggerShow.click();
+        statesListInput.setAttribute('required', '');
+    } else {
+        stateTriggerClose.click();
+        statesListInput.removeAttribute('required');
+    }
+});
--- step-form-logic.js
+++ step-form-logic.js
@@ -1,22 +1,22 @@
-// let appendWrapperStepButtons = document.querySelector('.steps-wrapper');
-// let allSteps = document.querySelectorAll('.step');
+let appendWrapperStepButtons = document.querySelector('.steps-wrapper');
+let allSteps = document.querySelectorAll('.step');
 
-// //формируем кнопочки шагов
-// allSteps.forEach(function(elem, index) {
-//     let stepWindowIndex = (index+1);
+//формируем кнопочки шагов
+allSteps.forEach(function(elem, index) {
+    let stepWindowIndex = (index+1);
 
-//     let clonableStepButton = document.querySelector('.step-button').cloneNode(true);
+    let clonableStepButton = document.querySelector('.step-button').cloneNode(true);
 
-//     if (index == 0) {
+    if (index == 0) {
         
-//     } else {
-//         clonableStepButton.classList.remove('step-button--active');
-//         clonableStepButton.classList.remove('step-button--current');
-//     }
+    } else {
+        clonableStepButton.classList.remove('step-button--active');
+        clonableStepButton.classList.remove('step-button--current');
+    }
 
-//     clonableStepButton.querySelector('.step-button__text').textContent = (index+1);
-//     appendWrapperStepButtons.append(clonableStepButton);
-// });
+    clonableStepButton.querySelector('.step-button__text').textContent = (index+1);
+    appendWrapperStepButtons.append(clonableStepButton);
+});
 
 
-// //отрабатываем клик по шагам
+//отрабатываем клик по шагам
